250c4c4471b8d43c2e07fb95445bf5a66b70c8aa
Jeff Paltera <jpaltera@vuestorefront.io>
2022-02-11 21:57:54 +0800

    chore: pr reviews resolve

--- packages/apollo/src/api/getPage.ts
+++ packages/apollo/src/api/getPage.ts
@@ -2,15 +2,7 @@ import { CustomQuery } from '@vue-storefront/core'
 import { gql } from '@apollo/client/core'
 import { ShopifyApolloContext } from '../library'
 import { QueryRoot, QueryRootPageArgs } from '../shopify'
-<<<<<<< HEAD
 import { GetPageParams } from '../types'
-=======
-
-interface GetPageParams {
-  id?: string
-  handle?: string
-}
->>>>>>> chore: update getPage to use apollo
 
 const defaultQuery = gql`
     query page(
@@ -19,10 +11,7 @@ const defaultQuery = gql`
     ) {
         page(handle: $handle, id: $id) {
           id
-<<<<<<< HEAD
           handle
-=======
->>>>>>> chore: update getPage to use apollo
           title
           onlineStoreUrl
           bodySummary
@@ -33,18 +22,9 @@ const defaultQuery = gql`
 `
 
 export default async function getPage(context: ShopifyApolloContext, params: GetPageParams, customQuery?: CustomQuery) {
-<<<<<<< HEAD
   const variables: QueryRootPageArgs = {
     ...(params.handle && { handle: params.handle }),
     ...(params.id && { id: params.id })
-=======
-  const variables: QueryRootPageArgs = {}
-
-  if (params.handle) {
-    variables.handle = params.handle
-  } else if (params.id) {
-    variables.id = params.id
->>>>>>> chore: update getPage to use apollo
   }
 
   const { page } = context.extendQuery(
@@ -57,17 +37,8 @@ export default async function getPage(context: ShopifyApolloContext, params: Get
     }
   )
 
-<<<<<<< HEAD
   return await context.client.apolloClient.query<QueryRoot, QueryRootPageArgs>({
     query: page.query,
     variables: page.variables
   }) ?? null
-=======
-  const response = await context.client.apolloClient.query<QueryRoot, QueryRootPageArgs>({
-    query: page.query,
-    variables: page.variables
-  })
-
-  return response ?? null
->>>>>>> chore: update getPage to use apollo
 }
